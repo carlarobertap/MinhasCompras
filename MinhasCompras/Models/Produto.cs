711dfa70b41422388df816be8fe9407b1d3e532e
Carla <carlaroberta.p1@hotmail.com>
2025-03-26 22:00:18 -0300

    Alteração do id string para int no Produto.cs e Construção da classe para fazer CRUD no SQLite

--- MinhasCompras/Models/Produto.cs
+++ MinhasCompras/Models/Produto.cs
@@ -5,7 +5,7 @@ namespace MinhasCompras.Models
     public class Produto
     {
         [PrimaryKey, AutoIncrement]
-        public string Id { get; set; }
+        public int Id { get; set; }
         public string Descricao { get; set; }
         public double Quantidade { get; set; }
         public double Preco {  get; set; }
