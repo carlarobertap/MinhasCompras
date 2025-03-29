c258a2a27f055663c2273e3148aeacf8a2be2922
Carla <carlaroberta.p1@hotmail.com>
2025-03-29 00:38:00 -0300

    Validação da inserção de produto e correção dos produtos duplicados na tela inicial

--- MinhasCompras/Models/Produto.cs
+++ MinhasCompras/Models/Produto.cs
@@ -4,11 +4,48 @@ namespace MinhasCompras.Models
 {
     public class Produto
     {
+        string _descricao;
+       
         [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
-        public string Descricao { get; set; }
-        public double Quantidade { get; set; }
-        public double Preco { get; set; }
+        public string Descricao {
+            get => _descricao;
+            set
+            {
+                if (value == null)
+                {
+                    throw new Exception("Por favor, preencha a descrição");
+                }
+
+                _descricao = value;
+
+            }
+        
+        }
+        double _quantidade;
+        public double Quantidade 
+        {
+            get => _quantidade;
+            set
+            {
+                if (value <= 0)
+                    throw new Exception("Por favor, preencha quantidade com um número maior que zero");
+
+                _quantidade = value;
+            }
+        }
+        double _preco;
+        public double Preco {
+            get => _preco;
+            set
+            {
+                if (value <= 0)
+                    throw new Exception("Por favor, preencha preço com um número maior que zero");
+
+                _preco = value;
+            }
+        }
+
         public double Total { get => Quantidade * Preco; }
     }
 }
